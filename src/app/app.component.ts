4f315c7276672b46a5a62200117b52493252b286
Irvin Medina <irvin.medina@softtek.com>
2022-10-03 17:39:23 -0500

    Programa Hola mundo

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Bienvenidos';
+  demo= 'Este es un demo de Git';
+  
+}
